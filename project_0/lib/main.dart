563f36989160bb5f497bf952743a4da558d5c81d
Antonio <antonio01guarini@gmail.com>
2025-07-31 12:06:36 +0200

    first api

--- project_0/lib/main.dart
+++ project_0/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:project_0/api/GameApi.dart';
 
 
 Future main() async {
@@ -17,7 +18,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: SizedBox(),
+      home: FutureBuilder(
+        future: GameApi().getGames(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return SizedBox(
+              child: Scaffold(
+                appBar: AppBar(
+                  title: Text('Flutter Riverpod Example'),
+                ),
+                body: Center(
+                  child: Text('Environment variables loaded successfully!'),
+                ),
+              ),
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
     );
   }
 }
